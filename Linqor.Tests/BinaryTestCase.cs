358bf3534915856639913b061ee8088fcee0b16f
mopozoff <morozovmav@gmail.com>
2015-02-09 19:15:49 +0300

    Added BinaryTestCase

---
+++ Linqor.Tests/BinaryTestCase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Linqor.Tests
+{
+    public static class BinaryTestCase
+    {
+        public static BinaryTestCase<T> Create<T>(string name, IEnumerable<T> outer, IEnumerable<T> inner, IEnumerable<T> expected)
+        {
+            return new BinaryTestCase<T>
+            {
+                Name = name,
+                Outer = outer,
+                Inner = inner,
+                Expected = expected
+            };
+        }
+    }
+    public class BinaryTestCase<T>
+    {
+        public string Name;
+        public IEnumerable<T> Outer;
+        public IEnumerable<T> Inner;
+        public IEnumerable<T> Expected;
+    }
+}
