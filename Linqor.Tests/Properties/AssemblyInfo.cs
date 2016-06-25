cb5d7ffca295f11e0f6453956def5bbf69ad4087
Andrew Morozov <morozovmav@gmail.com>
2016-06-25 21:09:03 +0300

    Incremented version

--- Linqor.Tests/Properties/AssemblyInfo.cs
+++ Linqor.Tests/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("5ec8f055-28d9-4b68-b19b-8d770d155588")]
 
-[assembly: AssemblyVersion("0.0.5.0")]
-[assembly: AssemblyFileVersion("0.0.5.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
