1d4d3f1d536c37aefa639e30a2155696d422cdbc
Andrew Morozov <morozovmav@gmail.com>
2016-07-02 21:01:45 +0300

    Release 1.0.0

--- Linqor/Properties/AssemblyInfo.cs
+++ Linqor/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("59a1192a-2bda-4585-86c4-3d3a45329986")]
 
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
